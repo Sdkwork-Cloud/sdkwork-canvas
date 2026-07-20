9df99bb5d8774bd325a2a54418e6506e051d5af9
zowork <sdkwork@zowalk.com>
2026-07-20 17:03:07 +0800

    chore: final sync (round 5)

--- crates/sdkwork-api-canvas-assembly/src/bootstrap.rs
+++ crates/sdkwork-api-canvas-assembly/src/bootstrap.rs
@@ -3,11 +3,11 @@
 use axum::Router;
 use sdkwork_canvas_pages_service::service::CanvasPagesService;
 
-pub struct ApplicationAssembly {
+pub struct ApiAssembly {
     pub router: Router,
 }
 
-pub fn assemble_application_business_router<R, D>(service: CanvasPagesService<R, D>) -> ApplicationAssembly
+pub fn assemble_api_router<R, D>(service: CanvasPagesService<R, D>) -> ApiAssembly
 where
     R: sdkwork_canvas_pages_service::ports::CanvasRepository,
     D: sdkwork_canvas_pages_service::ports::DrivePageContentPort,
@@ -15,7 +15,7 @@ where
     let app_router = sdkwork_routes_canvas_app_api::gateway_mount(service.clone());
     let backend_router = sdkwork_routes_canvas_backend_api::gateway_mount(service);
     let auth_router = sdkwork_routes_canvas_http_auth::gateway_mount();
-    ApplicationAssembly {
+    ApiAssembly {
         router: Router::new()
             .merge(app_router)
             .merge(backend_router)
